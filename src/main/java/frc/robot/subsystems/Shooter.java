c148ca9a25043a61bfd61c30d9c296339efe5564
Leona Wu <leonawu531@outlook.com>
2024-11-14 20:34:42 -0800

    Shooter Subsystem code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,69 @@
 package frc.robot.subsystems;
 
-public class Shooter {
-    
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase{
+    private static Shooter instance;
+    private Follower follow = new Follower(0, false);
+
+    public static Shooter getInstance(){
+        if (instance == null){
+            instance = new Shooter();
+        }
+        return instance;
+    }
+
+    private TalonFX m_shooterLeader;
+    private TalonFX m_shooterFollower;
+
+    public Shooter(){
+        m_shooterLeader = new TalonFX(0); //random values
+        m_shooterFollower = new TalonFX(1);
+        
+        configMotor(m_shooterLeader, false);
+        configMotor(m_shooterFollower, true);
+
+        m_shooterFollower.setControl(follow);
+    }
+
+    private void configMotor(TalonFX motor, boolean isInverted){
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        if(isInverted == true) {
+            config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        }
+        config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        
+    }
+
+    public enum ShooterStates{
+        ON(0.5),
+        OFF(0);
+
+        private double speed;
+
+        private double getValue(){
+            return speed;
+        }
+
+        ShooterStates(double speed) {
+            this.speed = speed;
+        }
+    }
+
+    public void setSpeed(ShooterStates state) {
+        m_shooterLeader.set(state.speed);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Speed Leader: ", m_shooterLeader.get());
+        SmartDashboard.putNumber("Speed Follower: ", m_shooterFollower.get());
+    }
 }
