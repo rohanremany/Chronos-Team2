aebca0005b32137128d49ae45794f3ed7dcbea80
Rohan Remany <114460210+rohanremany@users.noreply.github.com>
2024-12-03 18:59:43 -0800

    Intake
    Intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,48 @@
 package frc.robot.subsystems;
 
-public class Intake {
-    
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase{
+    private static Intake instance;
+    public static Intake getInstance() {
+        if(instance == null) {
+            instance = new Intake();
+        }
+        return instance;
+    }
+
+    private TalonFX m_intake;
+
+    public Intake() {
+        m_intake = new TalonFX(Constants.HardwarePorts.intake);
+    }
+
+    public enum IntakeSpeeds {
+        FORWARD(0.5),
+        OFF(0),
+        REV(-0.5);
+
+        private double speed;
+
+        private double getValue() {
+            return speed;
+        }
+
+        IntakeSpeeds(double speed) {
+            this.speed = speed;
+        }
+    }
+
+    public void setSpeed(IntakeSpeeds state) {
+        m_intake.set(state.speed);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Speed: ", m_intake.get());
+    }
 }
